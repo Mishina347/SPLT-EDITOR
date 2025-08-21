8b529af121d474a1b56a9b9b7cafdcd4e2933a5d
Mishina347 <mishima0013@gmail.com>
2025-08-22 02:23:52 +0900

    monaco worker追加

---
+++ src/useMonacoWorker.ts
@@ -0,0 +1,11 @@
+import * as monaco from 'monaco-editor'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+
+// @ts-ignore
+self.MonacoEnvironment = {
+	getWorker(_: any, label: string) {
+		return new editorWorker()
+	},
+}
+
+monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true)
